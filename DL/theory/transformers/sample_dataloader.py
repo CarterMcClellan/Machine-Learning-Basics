3908e8addaa1c6b8d6efd554ac83b112969e2dc3
Carter McClellan <carter@finesse.us>
2021-04-28 23:58:10 -0700

    refactor data downloader

---
+++ DL/theory/transformers/sample_dataloader.py
@@ -0,0 +1,3 @@
+# Transformers started with Machine Translation Datasets
+# So for all Transformer Notebooks, lets set up a simple Machine Translation task
+
--- Datasets/download_datasets.sh
+++
@@ -1,6 +0,0 @@
-if [ -e war_and_peace.txt ]
-then
-	echo "War and Peace Dataset already found"
-else
-	curl https://raw.githubusercontent.com/mmcky/nyu-econ-370/master/notebooks/data/book-war-and-peace.txt > war_and_peace.txt
-fi
