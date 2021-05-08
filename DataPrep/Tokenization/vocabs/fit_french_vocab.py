c2554002b1c4314f9b53cba4b9f10be56b842dcc
Carter McClellan <carter@finesse.us>
2021-05-08 03:33:07 -0700

    update vocab search

--- DataPrep/Tokenization/vocabs/fit_french_vocab.py
+++ DataPrep/Tokenization/vocabs/fit_french_vocab.py
@@ -1,10 +1,13 @@
 import os
 from pathlib import Path
 from pprint import pprint
+
 from tokenizers import ByteLevelBPETokenizer
 from tokenizers.pre_tokenizers import Whitespace
 
-def get_french_vocab():
+from transformers import PreTrainedTokenizerFast
+
+def get_french_vocab(model_name):
     root = Path(os.getcwd()).parent.parent.parent
     french_corpus = "Datasets/corpora/fr/text"
     fr_corpus_path = os.path.join(root, french_corpus)
@@ -23,7 +26,18 @@ def get_french_vocab():
                     min_frequency=2,
                     show_progress=True,
                     special_tokens=["<sos>", "<pad>", "<eos>", "<unk>"])
+    
+    print(tokenizer.encode("c'est la meilleure des phrases françaises").tokens)
+    tokenizer.save(model_name)
+
+def load_french_vocab(model_name):
+    #tokenizer = PreTrainedTokenizerFast(tokenizer_object=model_name)
+    #print(tokenizer.encode("c'est la meilleure des phrases françaises").tokens)
+    tokenizer = ByteLevelBPETokenizer("wiki_fr_tokenizer.json", add_prefix_space=True)
+    print(tokenizer.encode("c'est la meilleure des phrases françaises").tokens)
 
 
 if __name__ == "__main__":
-    get_french_vocab()
+    model_name = "wiki_fr_tokenizer.json"
+    #get_french_vocab(model_name)
+    load_french_vocab(model_name)
