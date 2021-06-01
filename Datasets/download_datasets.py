601b023431ff1629ffffb59b88bb2efbe66e2500
Carter McClellan <carter@finesse.us>
2021-05-31 23:05:27 -0700

    update to attenion notes

--- Datasets/download_datasets.py
+++ Datasets/download_datasets.py
@@ -5,10 +5,8 @@ from tqdm import tqdm
 
 import torchtext
 
-import spacy
-
-
 WAR_AND_PEACE = "https://raw.githubusercontent.com/mmcky/nyu-econ-370/master/notebooks/data/book-war-and-peace.txt"
+APPS = "https://people.eecs.berkeley.edu/~hendrycks/APPS.tar.gz"
 
 def download_from_url(url, filename):
     if os.path.exists(filename):
@@ -31,15 +29,13 @@ def download_IWSLT2016(target_dir='./IWSLT2016'):
 def download_wiki_corpora(language_code, dump):
     dump_name="{}wiki-{}-pages-articles-multistream.xml.bz2".format(language_code, dump)
     link="https://dumps.wikimedia.org/{}wiki/{}/".format(language_code, dump) + dump_name
-    download_from_url(link)
+    # download_from_url(link)
 
     # note: after downloading from wiki dumps
     # we are going to want to use wikiextractor
     # here: https://github.com/attardi/wikiextractor
 
-def download_spacy(model, path):
-    sputnik.install('spacy', spacy.about.__version__, model, data_path=path)
-
 if __name__ == "__main__":
     # download_wiki_corpora("fr", "20210120")
-    download_spacy('en_core_web_sm', os.getcwd())
+    download_from_url(WAR_AND_PEACE, "war_and_peace.txt")
+    download_from_url(APPS, "war_and_peace.txt")
